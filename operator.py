0ec653361c015dc1adb3f6562ab9a2c2eb792347
ziyad <mziyadbsk@gmail.com>
2024-09-28 19:57:45 +0300

    lesson 8

---
+++ operator.py
@@ -0,0 +1,6 @@
+name =str(input("enter your name "))
+age =int(input("enter your current age "))
+if name =="Ziyad" or name == "ziyad" or name == "Yashika"and age ==22 or name == "yashika"and age ==22:
+    print("welcome")
+else:
+   print("bye bye")
